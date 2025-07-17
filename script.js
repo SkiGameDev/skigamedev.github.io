91b10b0c236583097720d1c6f910dd8ffffd674a
SkiGameDev <147140781+SkiGameDev@users.noreply.github.com>
2025-07-17 11:59:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e){
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', e => {
     e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
+    document.querySelector(a.getAttribute('href'))
       .scrollIntoView({ behavior: 'smooth' });
   });
 });
